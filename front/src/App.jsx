ad1ad717251f33eb5c34842a7d34ef59bd14d6e9
Cristian8a <cris_17e@hotmail.com>
2024-02-03 16:11:04 -0600

    front

---
+++ front/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { MoviesGrid } from "./MoviesGrid";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "./App.module.css";
+
+import movies from "./movies.json"
+
+export function App() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+    };
+
+    return (
+        <div>
+            <header>
+                <h1 className={styles.title}>Movies</h1>
+            </header>
+            <main>
+                {/* Carrusel */}
+                <Slider {...settings} style={{ width: '80%', margin: '0 auto' }}>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            {/* Puedes renderizar el contenido del carrusel aquí */}
+                            <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+                            <h3 style={{ textAlign: 'start', fontSize: '20px' }}>{movie.title}</h3>
+                        </div>
+                    ))}
+                </Slider>
+
+                {/* Grid de películas */}
+                <MoviesGrid />
+            </main>
+        </div>
+    );
+}
