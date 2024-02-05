c1604380a6b8763cff80dfa1a800b095513c853b
Cristian8a <cris_17e@hotmail.com>
2024-02-04 23:57:20 -0600

    Pag&Details

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MoviesGrid } from "./MoviesGrid";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./App.module.css";
 
+import 'bulma/css/bulma.min.css';
 import movies from "./movies.json";
 import Navbar from './components/header/navbar';
+import { Pagination } from './components/header/Pagination';
+
+
 
 export function App() {
     const settings = {
@@ -16,6 +20,29 @@ export function App() {
         slidesToShow: 3,
         slidesToScroll: 3,
     };
+
+    // variables para paginado conectado a back
+    const [movie, setMovies] = useState([]);
+
+    const totalMovies = movie.length
+
+    const [moviesPerPage, setMoviesPerPage] = useState(6);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const lastIndex = currentPage * moviesPerPage // = 1 * 6 = 6
+    const firstIndex = lastIndex - moviesPerPage // = 6 - 6 = 0
+
+    // Obtener datos de la api desde el back
+    const movieList = async () => {
+        const data = await fetch('liga a back');
+        const movie = await data.json();
+
+        setMovies(movie);
+    }
+
+    useEffect(() => {
+        movieList();
+    }, []);
 
     return (
         <div>
@@ -32,11 +59,17 @@ export function App() {
                             <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
                             <h3 style={{ textAlign: 'start', fontSize: '20px' }}>{movie.title}</h3>
                         </div>
-                    ))}
+                    )).slice(firstIndex, lastIndex)}
                 </Slider>
 
                 {/* Grid de películas */}
                 <MoviesGrid />
+                <Pagination
+                    moviesPerPage={moviesPerPage}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    totalMovies={totalMovies}
+                />
             </main>
         </div>
     );
