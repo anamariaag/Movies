dde753386faae8258377a870812518a7db39dda4
IS729303 <is729303@iteso.mx>
2024-02-06 23:16:54 -0600

    Front limpio
    Front sin impresiones de consola

--- frontend/src/movieCard.jsx
+++ frontend/src/movieCard.jsx
@@ -1,51 +1,21 @@
-import React, { useState } from 'react';
-import ReactModal from 'react-modal';
-import styles from "./movieCard.module.css";
+import styles from "./movieCard.module.css"
 
 export function MovieCard({ movie }) {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    const openModal = () => setModalIsOpen(true);
-    const closeModal = () => setModalIsOpen(false);
-
-    const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
-
+    console.log(styles);
+    const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path
     return (
-        <div>
-            <li className={styles.movieCard} onClick={openModal}>
-                <img
-                    width={230}
-                    height={345}
-                    className={styles.movieImage}
-                    src={imageUrl}
-                    alt={movie.title}
-                />
-                <div>{movie.title}</div>
-                <div className={styles.details}>
-                    <div className={styles.price}> Price: {movie.popularity}</div>
-                    <div className={styles.status}> Status: {movie.vote_average}</div>
-                </div>
-            </li>
-
-            <ReactModal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                contentLabel="Movie Details Modal"
-            >
-                {/* Contenido del Modal */}
-                <h2>{movie.title}</h2>
-                <img
-                    width={230}
-                    height={345}
-                    src={imageUrl}
-                    alt={movie.title}
-                />
-                <div className={styles.details}>
-                    <div className={styles.price}> Price: {movie.popularity}</div>
-                    <div className={styles.status}> Status: {movie.vote_average}</div>
-                </div>
-                <button onClick={closeModal}>Cerrar modal</button>
-            </ReactModal>
-        </div>
+        <li className={styles.movieCard}>
+            <img width={230}
+                height={345}
+                className={styles.movieImage}
+                src={imageUrl}
+                alt="{movie.title}"
+            />
+            <div>{movie.title}</div>
+            <div className={styles.details}>
+                <div className={styles.price}> Price: {movie.popularity}</div>
+                <div className={styles.status}> Status: {movie.vote_average}</div>
+            </div>
+        </li>
     );
 }
